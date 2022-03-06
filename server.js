a2d5a2601830af7bdc9cf5ff6a3a21f74b8bdd2a
Digital Charlie <furman.jr@gmail.com>
2022-03-06 13:38:48 -0800

    added basic server.js code

---
+++ server.js
@@ -0,0 +1,44 @@
+/////////////////////////////////////////////
+// Import Our Dependencies 
+/////////////////////////////////////////////
+require('dotenv').config(); // Loads Env vars into process.env
+const express = require('express'); 
+const morgan = require('morgan');
+const methodOverride = require('method-override');
+const path = require('path');
+
+// const fruitController = require('./controllers/fruit')
+
+/////////////////////////////////////////////////
+// Create our Express Application Object Bind Liquid Templating Engine
+/////////////////////////////////////////////////
+const app = express()
+app.engine('jsx', require('express-react-views').createEngine());
+app.set('view engine', 'jsx')
+
+/////////////////////////////////////////////////////
+// Middleware
+/////////////////////////////////////////////////////
+app.use(morgan("tiny")); // logging
+app.use(express.urlencoded({extended:true}));  // parse urlencoded request bodies
+app.use(methodOverride("_method")); // override for put and delete requests from forms
+app.use(express.static("public")); // serve files from public statically
+
+
+/////////////////////////////////////////////////////
+// Routing
+/////////////////////////////////////////////////////
+
+app.get("/", (req,res) => {
+    res.send("Welcome to The Bargewright Inn")
+});
+
+//app.use('/user', userController); // tells user route to use user controller
+
+
+
+//////////////////////////////////////////////
+// Server Listener
+//////////////////////////////////////////////
+const PORT = process.env.PORT;
+app.listen(PORT, () => console.log(`Now Listening on port ${PORT}`));
