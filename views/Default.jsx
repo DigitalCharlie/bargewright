aa823b044fb6ac90b57b69aab009d8575eb5bd63
Digital Charlie <furman.jr@gmail.com>
2022-03-14 10:58:21 -0700

    breaking css into multiple files

--- views/Default.jsx
+++ views/Default.jsx
@@ -24,7 +24,9 @@ class Default extends React.Component {
                     <title>The Bargewright Inn</title>
                     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossOrigin="anonymous" />
                     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossOrigin="anonymous" />
-                    <link rel="stylesheet" href="/css/style.css" type="text/css"/> 
+                    <link rel="stylesheet" href="/css/baseline.css" type="text/css"/> 
+                    <link rel="stylesheet" href="/css/forms.css" type="text/css"/> 
+                    <link rel="stylesheet" href="/css/bodycontent.css" type="text/css"/> 
                 </head>
                 <body>
                     <header>
